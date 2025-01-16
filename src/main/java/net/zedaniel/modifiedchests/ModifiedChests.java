921deed1aadf94a79d641ec65bbce81cf1f57646
Dayane kelly <54118755+daykell1@users.noreply.github.com>
2025-01-16 16:27:42 -0300

    Nomeando mod

--- src/main/java/net/zedaniel/modifiedchests/ModifiedChests.java
+++ src/main/java/net/zedaniel/modifiedchests/ModifiedChests.java
@@ -1,4 +1,4 @@
-package net.zedaniel.tutorialmod;
+package net.zedaniel.modifiedchests;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,14 +14,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod {
+@Mod(ModifiedChests.MOD_ID)
+public class ModifiedChests {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "modifiedchests";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TutorialMod() {
+    public ModifiedChests() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
